db5684672d858f53b4756f76bfb6d58aef165a5c
protarie <protarie@gmail.com>
2023-03-09 11:39:13 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	ll n; cin >> n;
+	cout << n * n * n << '\n' << 3;
+
+	return 0;
+}
